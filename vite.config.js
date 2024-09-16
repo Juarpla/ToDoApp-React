8b895d2bc8bbe926ace358ea507fc7337ea4b7c5
Juan Plasencia <jplasencia.dev@outlook.com>
2024-09-16 06:46:12 -0500

    add final deployment of automation

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/todapp-react/",
+  base: "https://juarpla.github.io/todapp-react/",
 });
