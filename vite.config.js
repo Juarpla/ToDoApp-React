8836633a253061455681ecb4bde3ea59e4fd7405
Juan Plasencia <jplasencia.dev@outlook.com>
2024-09-16 07:56:59 -0500

    vite config to main branch

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
+// base: "https://juarpla.github.io/todapp-react/" for gh-pages branch
 export default defineConfig({
   plugins: [react()],
-  base: "/todapp-react/",
+  base: "/dist/",
 });
